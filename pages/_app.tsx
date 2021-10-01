4efbbd90235bdbead20a0ebcd6954e43db5681c2
Abdul Muqeet <39890909+abdulmuqeet1@users.noreply.github.com>
2021-10-01 23:54:51 +0500

    replacing react-redux with use context api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,32 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  createContext,
+} from "react";
 import Layout from "../components/layout/layout";
 
+const Usercontext = createContext({});
+
 function MyApp({ Component, pageProps }: AppProps) {
+  // shift this user cookie code to layout file
+  const [user, setUser] = useState("abdul");
+  useEffect(() => {
+    if (window.localStorage.getItem("cp_uid")) {
+      let user = window.localStorage.getItem("cp_uid");
+      setUser(user!);
+    }
+  }, [user]);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Usercontext.Provider value={user}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Usercontext.Provider>
   );
 }
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
+import style from "../styles/Home.module.scss";
+import { useState, useEffect, useContext } from "react";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={style.maincontainer}>
       <h2>Main Page</h2>
       <p>pallete page </p>
       <p>color library page</p>
